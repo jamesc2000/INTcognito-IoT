2a0586ceb740833f46b810b3985f6d80259343f5
James <cruz.james99@gmail.com>
2022-11-20 00:49:40 +0800

    + ESP OTA enabled + Arduino sensor script initial + ESP GPIO working
    TODO:
    1. Arduino clean up signal from sudden changes in value

---
+++ Arduino-Ultrasonic/Arduino-Ultrasonic.ino
@@ -0,0 +1,43 @@
+#define echoPin 8 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 9 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define statusPin 5 // high if door is open, low if door is closed, NOTE: This will be 5V, esp32 needs 3.3V double check first, do not plug directly
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(statusPin, OUTPUT);
+  pinMode(13, OUTPUT); // Turn off builtin LED
+  digitalWrite(13, LOW);
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  Serial.println("with Arduino UNO R3");
+}
+void loop() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (distance > 10) {
+    Serial.println("Door open");
+    digitalWrite(statusPin, HIGH);
+  } else {
+    Serial.println("Door closed");
+    digitalWrite(statusPin, LOW);
+  }
+}
--- INTcognito-IoT.ino
+++ INTcognito-IoT.ino
@@ -1,21 +1,158 @@
-#define BLYNK_TEMPLATE_ID "TMPLohv5UbVV"
-#define BLYNK_DEVICE_NAME "James ESP32"
+#define BLYNK_PRINT Serial
 
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+#define BLYNK_TEMPLATE_ID "TMPLohv5UbVV"
+#define BLYNK_DEVICE_NAME "Test"
+#define BLYNK_AUTH_TOKEN "ni4uHKC-iMnNNLEq4aeQO0gDtsjpYzWq"
+
+#include <BlynkSimpleEsp32.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
 #include <Arduino.h>
 
-#define LED 2
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "ni4uHKC-iMnNNLEq4aeQO0gDtsjpYzWq";
 
-void setup()
-{
-    pinMode(LED, OUTPUT);
+const char* host = "esp32";
+char ssid[] = "HUAWEI-V4XU";
+char pass[] = "Tataycruz";
+
+WebServer server(80);
+
+#include "otaLoginPage.h"
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent() {
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, millis() / (1000));
 }
 
-void loop()
-{
-    delay(1000);
-    Serial.println("On");
-    digitalWrite(LED, HIGH);
-    delay(1000);
-    Serial.println("Off");
-    digitalWrite(LED, LOW);
+// Pin Assignments
+constexpr int DOOR_SNS = 2;
+constexpr int INDICATOR_LED = 33;
+
+void setup() {
+  // PIN Modes
+  pinMode(DOOR_SNS, INPUT);
+  pinMode(INDICATOR_LED, OUTPUT);
+  
+  Serial.begin(9600);
+
+  // Connect to WiFi network
+  WiFi.begin(ssid, pass);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  /*use mdns for host name resolution*/
+  if (!MDNS.begin(host)) { //http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex);
+  });
+  server.on("/serverIndex", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+  server.begin();
+
+  Blynk.config(auth);
+  Blynk.connect(30);
+  if (Blynk.connected()) {
+    Serial.println("Successfully connected to Blynk");
+  } else {
+    Serial.println("Blynk connection request failed");
+  }
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
 }
+
+void loop() {
+  server.handleClient();
+  delay(1);
+  if (Blynk.connected()) {
+    Blynk.run();
+    timer.run(); // Initiates BlynkTimer
+  }
+
+  if (digitalRead(DOOR_SNS) == 1) {
+    digitalWrite(INDICATOR_LED, LOW); // Inverted logic for indicator led gpio 33
+  } else {
+    digitalWrite(INDICATOR_LED, HIGH);
+  }
+  
+  //Serial.println("Message: ");
+  //Serial.println(Serial.readString());
+  // // Clears the trigPin condition
+  // digitalWrite(trigPin, LOW);
+  // delayMicroseconds(2);
+  // // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  // digitalWrite(trigPin, HIGH);
+  // delayMicroseconds(10);
+  // digitalWrite(trigPin, LOW);
+  // // Reads the echoPin, returns the sound wave travel time in microseconds
+  // duration = pulseIn(echoPin, HIGH);
+  // // Calculating the distance
+  // distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // // Displays the distance on the Serial Monitor
+  // Serial.print("Distance: ");
+  // Serial.print(distance);
+  // Serial.println(" cm");
+}
+
+
+// TODO:
+// Run the sensor at 3.3V
+// MEASURE FIRST THE ECHO PIN!! MAKE SURE IT DOESNT GO 5V
